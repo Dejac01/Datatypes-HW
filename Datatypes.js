e972e49dbc7a0ccbe75deef5deadee4de231d038
Dejac01 <dejacraft28@gmail.com>
2023-01-22 20:38:11 -0500

    first commit

---
+++ Datatypes.js
@@ -0,0 +1,158 @@
+//String 
+
+let myname="Deja"
+console.log("Hi My Name is", myname);
+
+let myfavecolor="Red"
+console.log("My favorite color is" , myfavecolor);
+
+let hometown="New Orleans"
+console.log("I am from", hometown);
+
+let myfavefood="Seafood"
+console.log("My favorite food is", myfavefood);
+
+let myfavebook="The Alchemist"
+console.log("my favorite book is" , myfavebook);
+
+let mypet="Pomeranian named Zues"
+console.log("I have a",mypet);
+
+let dreamvacation="A Luxury Train Ride"
+console.log("my dream vacation is" , dreamvacation);
+
+let hobby="Reading"
+console.log("My favorite hobby is", hobby);
+
+let pastime="Sleep"
+console.log("I also like to", pastime);
+
+let goal="Game Developer"
+console.log("One day I hope to become a", goal);
+
+// Integers
+
+const one = "1"
+console.log(one)
+
+const Two = "2"
+console.log(Two)
+
+const three = "3"
+console.log(three)
+
+const four = "4"
+console.log(four)
+
+const five = "5"
+console.log(five)
+
+const six = "6"
+console.log(six)
+
+const seven= "7"
+console.log(seven)
+
+const eight = "8"
+console.log(eight)
+
+const nine = "9"
+console.log(nine)
+
+const Ten = "10"
+console.log(Ten)
+
+// Arrays
+
+let luxurycars = [
+    "Porshe",
+    "Mercedes",
+    "BMW" ];
+console.log(luxurycars)
+
+  let southernstates = [
+    "Georgia",
+    "Florida",
+    "Louisiana"];
+console.log(southernstates)
+
+let countries =  [
+    "America",
+    "Russia",
+    "Japan"];
+    console.log(countries)
+
+
+  let hobbies = [
+    "Reading",
+    "Drawing",
+    "Sewing" ];
+    console.log(hobbies)
+
+    let sports = [
+        "Basketball",
+        "Football",
+        "Soccer" ];
+        console.log(sports)
+
+        let colors = [
+          "Red",
+          "Blue",
+          "Purple" ];
+          console.log(colors)
+
+          let singers = [
+            "Sade",
+            "Snoh Aalegra",
+            "Jhene Aiko" ];
+            console.log(singers)
+
+            let movies= [
+              "Black Panther",
+              "Transformers",
+              "Doctor Strange" ];
+              console.log(movies)
+
+              let luxurybrands = [
+                "Hermes",
+                "Chanel",
+                "Louis Vuitton" ];
+                console.log(luxurybrands)
+
+                let shoebrands = [
+                  "Nike",
+                  "New Balance",
+                  "Adidas" ];
+                  console.log(shoebrands)
+
+//objects
+
+let handbag1 = {type:"messenger bag", brand:"steve madden", color:"white"};
+console.log(handbag1)
+
+let handbag2 = {type:"hobo bag", brand:"Aldo", color:"Pink"};
+console.log(handbag2)
+
+let handbag3 = {type:"Hermes Kelly", brand:"Hermes", color:"black"};
+console.log(handbag3)
+
+let handbag4 = {type:"cleo bag", brand:"Prada", color:"Red"};
+console.log(handbag4)
+
+let handbag5 = {type:"Horsebit bag", brand:"Gucci", color:"multi-color"};
+console.log(handbag5)
+
+let handbag6 = {type:"Park tote", brand:"the row", color:"brown"};
+console.log(handbag6)
+
+let handbag7 = {type:"Sicily bag", brand:"Dolce", color:"Black"};
+console.log(handbag7)
+
+let handbag8 = {type:"Twist MM", brand:"Louis Vuitton", color:"Green"};
+console.log(handbag8)
+
+let handbag9 = {type:"top-handled leather bag", brand:"Versace", color:"Pink"};
+console.log(handbag9)
+
+let handbag10 = {type:"Book Tote", brand:"Dior", color:"multicolor"};
+console.log(handbag10)
